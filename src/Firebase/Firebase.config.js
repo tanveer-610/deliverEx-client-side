e6fc3e09ac619dcb5aba2222a7a70b7f23e88d5f
tanveer-610 <tanvirul35-2779@diu.edu.bd>
2021-12-03 00:17:40 +0600

    update

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,9 +1,4 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCYldGSLPJ_D59I93x3pVhi7FWRCKiQFXc",
-    authDomain: "deliveryexauthentication.firebaseapp.com",
-    projectId: "deliveryexauthentication",
-    storageBucket: "deliveryexauthentication.appspot.com",
-    messagingSenderId: "386094737157",
-    appId: "1:386094737157:web:678c60964c32bb63f5b96c"
+    /* add your token*/
 };
 export default firebaseConfig;
